7cc995db9d0081b1cbb9beda77b14b93b76ce28c
Bill Dapper <dkw.dapper@gmail.com>
2024-04-14 23:36:52 +0100

    encrypt function updated

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,19 +1,41 @@
-def encrypt(myText, shift):
-    newText = ''
-    for letter in myText:
-        indexNumber = alphabet.index(letter)
-        newIndex = indexNumber + shift
-        if newIndex >= 25:
-            newIndex -= 26
-        newText += alphabet[newIndex]
-#        print(f"{indexNumber} \n")
-#        print(f"The {indexNumber} which is {indexNumber}  - - has an {encodedNumber}so the new string is --{newText}")
-    print(f"{newText} \n")
+from art import logo
+print(logo)
+
+def encrypt(encryption_type, my_text, shift):
+    new_text = ''
+    if shift > 26:
+        shift = shift % 26
+    if encryption_type == 'encode':
+        for char in my_text:
+            if char in alphabet:
+                indexNumber = alphabet.index(char)
+                newIndex = indexNumber + shift
+                if newIndex >= 26:
+                    newIndex -= 26
+                new_text += alphabet[newIndex]
+            else:
+                new_text += char
+        print(f"Encrypted letter of {my_text} is {new_text} \n")
+    elif encryption_type == 'decode':
+        for char in my_text:
+            if char in alphabet:
+                for letter in my_text:
+                    indexNumber = alphabet.index(letter)
+                    newIndex = indexNumber - shift
+                    if newIndex < 0:
+                        newIndex += 26
+                    new_text += alphabet[newIndex]
+            else:
+                new_text += char
+            print(f"Decrypted letter of {my_text} is {new_text} \n")
+    else:
+        print("You chose an incorrect number for the encryption")
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt and 'decode' to decrypt: \n")
-text = str(input("Enter a word to encode: \n"))
+direction = str(input("Type 'encode' to encrypt and 'decode' to decrypt: \n")).lower()
+text = str(input("Enter a word to encode: \n")).lower()
+continuation = str(input("Do you wish to continue this game: \n"))
 shiftNumber = int(input("Enter a number to shift: \n"))
-encrypt(myText=text, shift=shiftNumber)
+encrypt(encryption_type=direction, my_text=text, shift=shiftNumber)
