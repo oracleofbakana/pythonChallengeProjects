d8df11c48ec44d70cf1d7fa810dab9aff48d67a2
Bill Dapper <dkw.dapper@gmail.com>
2024-04-22 15:16:27 +0100

    day 12 - scopes and block

--- blackstone.py
+++ blackstone.py
@@ -1,4 +1,5 @@
 import random
+from art import blackjack
 
 def deal_cards():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -32,37 +33,41 @@ def calculate_score(cards):
 
     return sum(cards)
 
-
-user_cards = []
-computer_cards = []
-is_game_over = False
-computer_score = 0
-
-
-for _ in range(2):
-    user_cards.append(deal_cards())
-    computer_cards.append(deal_cards())
+def playgame():
+    print (blackjack)
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+    computer_score = 0
 
 
-while not is_game_over:
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
-    print(f"Your cards is : {user_cards} and score is {user_score}")
-    print(f"Computer cards: {computer_cards}. ")
+    for _ in range(2):
+        user_cards.append(deal_cards())
+        computer_cards.append(deal_cards())
 
-    if user_score == 0 or computer_score == 0 or user_score > 21:
-        is_game_over = True
-    else:
-        user_should_deal = input(f"Type 'y' to continue and 'n' to exit.:\n")
-        if user_should_deal == 'y':
-            user_cards.append(deal_cards())
-        else:
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"Your cards is : {user_cards} and score is {user_score}")
+        print(f"Computer cards: {computer_cards}. ")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
             is_game_over = True
+        else:
+            user_should_deal = input(f"Type 'y' to continue and 'n' to exit.:\n")
+            if user_should_deal == 'y':
+                user_cards.append(deal_cards())
+            else:
+                is_game_over = True
 
-while computer_score != 0 and computer_score < 17:
-    computer_cards.append(deal_cards())
-    computer_score = calculate_score(computer_cards)
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_cards())
+        computer_score = calculate_score(computer_cards)
 
-print(f"Your final hand: {user_cards} and final score: {user_score}")
-print(f"Computer's final hand: {computer_cards} and final score: {computer_score}")
-print(compare(user_score,computer_score))
+    print(f"Your final hand: {user_cards} and final score: {user_score}")
+    print(f"Computer's final hand: {computer_cards} and final score: {computer_score}")
+    print(compare(user_score,computer_score))
+
+while input(f"Type 'y' to play the blackjack game and 'n' to exit:\n") == "y":
+    playgame()
---
+++ number_guess_game.py
@@ -0,0 +1,55 @@
+import random
+from art import number_guess
+
+
+def random_pick():
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+    user_guess = random.choice(numbers)
+    return user_guess
+
+
+def compare(number_picked, user_guess):
+    if user_guess > 50:
+        return 4
+    elif user_guess == number_picked:
+        return 1
+    elif user_guess > number_picked:
+        return 2
+    elif user_guess < number_picked:
+        return 3
+
+
+
+def user_trial():
+    trial = int(input(f"To play the game, enter a number: /n"))
+    return trial
+
+
+def play_game():
+    print(number_guess)
+    computer_pick = random_pick()
+    difficulty = str(input(f"Choose your difficulty level. Type 'hard' for hard and 'easy' for easy: \n"))
+    attempt = 0
+    if difficulty == 'hard':
+        attempt = 5
+    else:
+        attempt = 10
+
+    while attempt >= 1:
+
+        user_pick = user_trial()
+        result = compare(computer_pick, user_pick)
+
+        if result == 1:
+            print(f"You won the game. You picked the correct number")
+            if input(f"Do you want to play again: Type 'y' to continue") == 'y': play_game()
+        elif result == 2:
+            print(f"Number is too high. You have {attempt - 1} left")
+        elif result == 3:
+            print(f"Number is too low. You have {attempt - 1} left")
+        elif result == 4:
+            print(f"Your guess should be between 1 and 50. You have {attempt - 1} left")
+        attempt -= 1
+
+while input(f"Type 'y' to play the number guess game and 'n' to exit:\n") == "y":
+    play_game()
